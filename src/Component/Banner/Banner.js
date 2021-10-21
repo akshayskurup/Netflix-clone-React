12a9997d2147891a9cce948b07937d903a2e75de
Akshay S Kurup <akshayskurup@gmail.com>
2021-10-21 20:25:33 +0530

    Completed

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -3,22 +3,25 @@ import './Banner.css'
 import {API_key,imgUrl} from '../../constant/constant'
 import axios from '../../axios'
 
+
 function Banner() {
          const [Movie, setMovie] = useState()
     useEffect(() => {
         axios.get(`trending/all/week?api_key=${API_key}&language=en-US`).then((Response)=>{
             console.log(Response.data.results[10])
-            setMovie((Response.data.results[11]))
+           setMovie((Response.data.results[1]))
         })
     }, [])
     return (
         <div style={{backgroundImage:`url(${Movie ? imgUrl+Movie.backdrop_path : ""})`}} className='banner'>
             <div className='content'>
                 <h1 className='title'>{Movie ? Movie.original_title:'Movie.name'}</h1>
+                
                 <div className='banner_buttons'>
-                    <button className='button'>Play</button>
+                    <button className='button'> <img className='button-icon' src="\icon\play-icon-white.png" alt="" />Play</button>
                     <button className='button'>My List</button>
                 </div>
+                
                 <h1 className='discription'>{Movie ? Movie.overview:""}</h1>
             </div>
             <div className="fade_bottom"></div>
